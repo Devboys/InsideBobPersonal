b0c0eb7c5bb463e52bf4e7aa40262ad5074a64c8
Jonathan <jonathan_hertz2@live.dk>
2019-11-16 16:58:16 +0100

    Fixed EnemyBounce

--- Assets/Scripts/EnemyBounce.cs
+++ Assets/Scripts/EnemyBounce.cs
@@ -21,18 +21,22 @@ public class EnemyBounce : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        var pc = collision.gameObject.GetComponent<OldPlayerController>();
+        var pc = collision.gameObject.GetComponent<PlayerController>();
         if (pc)
         {
             if (pc.IsCannonBall()) Die();
         }
 
-        RaycastHit2D[] hitInfos = new RaycastHit2D[1];
-        collision.Raycast(rb.velocity, hitInfos, Mathf.Infinity, platformLayer);
-
-        if (hitInfos.Length > 0)
+        Vector2[] rayDir = new Vector2[] { Vector2.up, Vector2.left, Vector2.down, Vector2.right };
+        foreach (Vector2 dir in rayDir)
         {
-            rb.velocity = Vector2.Reflect(rb.velocity, hitInfos[0].normal);
+            RaycastHit2D[] hitInfos = new RaycastHit2D[1];
+            col.Raycast(dir, hitInfos, col.radius + 0.1f, platformLayer);
+            if (hitInfos.Length > 0 && hitInfos[0].point != Vector2.zero)
+            {
+                Debug.Log(hitInfos[0].point);
+                rb.velocity = Vector2.Reflect(rb.velocity, -dir).normalized * magnitude;
+            }
         }
     }
 
