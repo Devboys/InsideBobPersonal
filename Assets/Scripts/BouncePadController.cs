d979fc5a20d647fa061d67df46fc98b73e140248
Jeppe Stougaard Faber <jepf@itu.dk>
2019-11-18 14:17:57 +0100

    dot product cutoff on bouncepad is now adjustable

--- Assets/Scripts/BouncePadController.cs
+++ Assets/Scripts/BouncePadController.cs
@@ -14,7 +14,13 @@ public class BouncePadController : MonoBehaviour
     public string bounceSound;
 
     public Vector2 direction;
-    
+    public float dotCutoff = 0.05f;
+
+    private void Start()
+    {
+        dotCutoff = Mathf.Abs(dotCutoff);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -23,20 +29,23 @@ public class BouncePadController : MonoBehaviour
             Vector2 reflectedVelocity = Vector2.Reflect(player.velocity, transform.up).normalized;
             float dot = Vector2.Dot(player.velocity.normalized, reflectedVelocity);
 
-            Debug.Log(dot);
             if (fixedDirection)
             {
                 Vector2 dir = direction.normalized;
                 dir.x = Mathf.Sign(transform.up.x) * dir.x;
                 player.StartBounce(dir);
+
+                //Play bounce sound.
+                RuntimeManager.PlayOneShot(bounceSound, transform.position);
             }
-            else if(dot < 0.95 || dot > 1.05) 
+            else if(dot < 1 - dotCutoff || dot > 1 + dotCutoff) 
             {
                 player.StartBounce(reflectedVelocity);
+
+                //Play bounce sound.
+                RuntimeManager.PlayOneShot(bounceSound, transform.position);
             }
             
-            //Play bounce sound.
-            RuntimeManager.PlayOneShot(bounceSound, transform.position);
         }
     }
 
