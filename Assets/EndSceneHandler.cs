338e78c3a56faf7d2a8185ae9a48d6a1fb0ec4cf
Magnus0808 <magnusrp0808@gmail.com>
2019-12-19 21:58:21 +0100

    Added Credits End thingy

---
+++ Assets/EndSceneHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSceneHandler : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayEndScene()
+    {
+        GameObject.FindGameObjectWithTag("UI").SetActive(false);
+        gameObject.SetActive(true);
+        animator.SetTrigger("Play");
+    }
+
+    void GoToTitle()
+    {
+        SceneManager.LoadScene("Scenes/TitleScene");
+    }
+
+}
---
+++ Assets/PlayEndScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEndScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Resources.FindObjectsOfTypeAll<EndSceneHandler>()[0].PlayEndScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
